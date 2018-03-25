100c17e1e5a17ba8fe0aa6baf25bd77be76fbb53
Johnson <jan_319@msn.com>
2018-03-26 01:48:13 +0300

    update
    reduced knocking volume

--- addons/afi_knocking/config.cpp
+++ addons/afi_knocking/config.cpp
@@ -83,7 +83,7 @@ class CfgSounds {
 	sounds[] = {"bridge_kn_knockMetal"};
 	class bridge_kn_knockMetal {
 		name = "bridge_kn_knockMetal";
-		sound[] = {"\afi_knocking\sounds\knockMetal.ogg",25,1,25};
+		sound[] = {"\afi_knocking\sounds\knockMetal.ogg",25,0.5};
 		titles[] = {};
 	};
 	class bridge_kn_knockMetalInside {
